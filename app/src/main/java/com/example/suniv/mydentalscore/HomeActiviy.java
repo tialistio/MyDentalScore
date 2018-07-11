da9aba3716c49ffafca9dd699e95b101c4621832
tialistio <tia.listio@gmail.com>
2018-07-11 11:10:19 +0900

    add scoring activity

--- app/src/main/java/com/example/suniv/mydentalscore/HomeActiviy.java
+++ app/src/main/java/com/example/suniv/mydentalscore/HomeActiviy.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HomeActiviy extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -41,6 +43,18 @@ public class HomeActiviy extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        TextView login = (TextView)findViewById(R.id.tv_login);
+
+        login.setMovementMethod(LinkMovementMethod.getInstance()); //---> add underline when clicked
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActiviy.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -52,6 +66,8 @@ public class HomeActiviy extends AppCompatActivity
             super.onBackPressed();
         }
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -84,14 +100,16 @@ public class HomeActiviy extends AppCompatActivity
         if (id == R.id.nav_home) {
             // Handle the camera action
         } else if (id == R.id.nav_scooring) {
-
+            Intent intent = new Intent(this, Scoring_1.class);
+            startActivity(intent);
         } else if (id == R.id.nav_source) {
 
         } else if (id == R.id.nav_result) {
             Intent intent = new Intent(this, ResultActivity.class);
             startActivity(intent);
         } else if (id == R.id.nav_login) {
-
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_exit) {
 
         }
